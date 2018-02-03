1cd7dc7776162337866483a972da3b045a89ddd6
Micah Akpan <mickey4sure2k7@gmail.com>
2018-02-03 17:48:31 +0100

    Add login feature

--- web_server.py
+++ web_server.py
@@ -95,6 +95,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><head><style>%s</style></html><body>" % styles
                 output += "<h1>Welcome to my page dear user!</h1>"
+                output += "<p><a href='/users'>Create a new Account</a></p>"
+                output += "<p><a href='/users/login'>Sign In</a></p>"
                 output += "</body></html>"
                 self.wfile.write(output)
                 return
@@ -137,7 +139,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 </div>
 
                 <div>
-                  <input type='submit' value='Sign Up'>
+                  <input type='submit' value='Login'>
                 </div>
                 '''
                 output += "</body></html>"
@@ -168,7 +170,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     # find a user with that username, email or password
                     print (username, email, password)
 
-                    print (or_)
+                    # print (or_)
                     # a_user = session.query(User).filter_by(or_(user_name = username[0],
                     #                                       email = email[0], password = password[0]))
 
@@ -179,7 +181,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     by_email = session.query(User).filter_by(email = email[0]).all()
 
                     # print (a_user)
-                    new_output = ""
+                    # new_output = ""
 
                     if ((by_username != []) or (by_email != [])):
                         # user exists, send a error message
@@ -194,7 +196,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                             message = 'username'
                             data = by_username[0].user_name
 
-                        print ('user exists...')
+                        # print ('user exists...')
+                        new_output = ""
                         new_output += "<html><head><style>%s</style></head><body>" % css_styles()
                         new_output += "<h1>Error!</h1><p>A user with the %s: %s already exists</p>" % (message, data)
                         new_output += "<a href='/users'>Go Back</a>"
@@ -222,6 +225,38 @@ class webServerHandler(BaseHTTPRequestHandler):
                     self.wfile.write(output)
                     # print output
                     return
+
+            if self.path.endswith("/login"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == "multipart/form-data":
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+
+                    email = fields.get('useremail')
+                    password = fields.get('user-pwd')
+
+                    user = session.query(User).filter_by(email=email[0], password=encrypt(password[0])).all()
+
+                    print (user)
+
+                    if (user == []):
+
+                        # user does not exist
+                        # redirect user to the login page
+
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/login')
+                        self.end_headers()
+                        self.wfile.write(output)
+                        return
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/')
+                    self.end_headers()
+                    self.wfile.write(output)
+                    return
+
 
         except:
             pass
